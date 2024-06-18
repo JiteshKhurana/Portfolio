c10fbea404165245ab504928db72305f42872e36
Jitesh Khurana <jkhurana_be21@thapar.edu>
2024-06-19 02:20:45 +0530

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Work_Sans } from 'next/font/google'
+import './globals.css'
+
+const workSans = Work_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'John Doe',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={workSans.className}>{children}</body>
+    </html>
+  )
+}
