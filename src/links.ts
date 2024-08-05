c113b71a9d1477135a739b05db70f0e665029984
Jitesh Khurana <jkhurana_be21@thapar.edu>
2024-08-05 15:34:53 +0530

    removed unnecessary links

--- src/links.ts
+++ src/links.ts
@@ -1,8 +1,6 @@
 import github from '../public/icons/github.svg'
-import instagram from '../public/icons/instagram.svg'
 import linkedin from '../public/icons/linkedin.svg'
 import readcv from '../public/icons/read-cv.svg'
-import x from '../public/icons/x.svg'
 import code from '../public/icons/code.svg'
 import leetcode from '../public/icons/leetcode.svg'
 
@@ -15,21 +13,14 @@ type Link = {
   text?: string
 }
 
-const websites = [
-  'github',
-  'instagram',
-  'linkedin',
-  'Resume',
-  'x',
-  'leetcode',
-  'projects',
-]
+const websites = ['github', 'linkedin', 'Resume', 'leetcode', 'projects']
 
 const LINKS: { [key in SocialMedia]: Link } = {
-  projects: {
-    title: 'Projects',
-    icon: code,
-    link: '/projects',
+  leetcode: {
+    title: 'Leetcode',
+    icon: leetcode,
+    link: 'https://leetcode.com/u/Jiteshkhurana/',
+    text: '@Jiteshkhurana',
   },
   github: {
     title: 'Github',
@@ -37,35 +28,22 @@ const LINKS: { [key in SocialMedia]: Link } = {
     link: 'https://github.com/JiteshKhurana',
     text: '@JiteshKhurana',
   },
+  projects: {
+    title: 'Projects',
+    icon: code,
+    link: '/projects',
+  },
   linkedin: {
     title: 'Linkedin',
     icon: linkedin,
     link: 'https://www.linkedin.com/in/jitesh-khurana/',
     text: '@jitesh-khurana',
   },
-  x: {
-    title: 'X',
-    icon: x,
-    link: 'https://twitter.com/jiteshKhurana12',
-    text: 'jiteshKhurana12',
-  },
-  instagram: {
-    title: 'Instagram',
-    icon: instagram,
-    link: 'https://www.instagram.com/jiteshkhurana_/',
-    text: '@jiteshkhurana_',
-  },
   resume: {
     title: 'Resume',
     icon: readcv,
     link: 'https://drive.google.com/file/d/13HtvJ2NVljJSv0Kx_M7ljnMxhVDCMlsJ/view?usp=sharing',
     text: '@jiteshkhurana',
-  },
-  leetcode: {
-    title: 'Leetcode',
-    icon: leetcode,
-    link: 'https://leetcode.com/u/Jiteshkhurana/',
-    text: '@Jiteshkhurana',
   },
 }
 
